c001268a43f2356ea558ac621c727bf9260a4ef8
guanly <guanly2010@126.com>
2015-11-15 22:22:25 +0800

    guanly test

---
+++ src/main/java/Test.java
@@ -0,0 +1,9 @@
+/**
+ * Created by gly on 2015/11/15.
+ */
+public class Test {
+    public static void main(String[] args) {
+        User user = new User("guanly", 1L);
+        System.out.println(user);
+    }
+}
