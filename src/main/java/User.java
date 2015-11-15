e956836fe829bc8a5060ba2944d2a3e3c2bdc34d
guanly <guanly2010@126.com>
2015-11-15 22:25:38 +0800

    guanly test

---
+++ src/main/java/User.java
@@ -0,0 +1,12 @@
+/**
+ * Created by gly on 2015/11/15.
+ */
+public class User {
+    private long id;
+    private String name;
+
+    public User(String name, long id) {
+        this.name = name;
+        this.id = id;
+    }
+}
